31b85205904509a711ad82ab6a4be0dac3413c17
thallys <thallys.rc@gmail.com>
2022-05-19 21:40:58 -0300

    hashtable c with linear search

--- c/hashtable.c
+++ c/hashtable.c
@@ -6,6 +6,7 @@
 
 #define MAX_NAME 256
 #define TABLE_SIZE 10
+#define DELETED_NODE (person*)(0xFFFFFFFF)
 
 /* git copilot is unbelievable*/
 
@@ -35,9 +36,14 @@ unsigned int hash(char *name) {
 }
 
 void print_table() {
+	printf("START HASH TABLE\n");
 	for (int i = 0; i < TABLE_SIZE; i++) {
-		if (hash_table[i] != NULL) {
-			printf("%d: %s\n", i, hash_table[i]->name);
+		if (hash_table[i] == NULL) {
+			printf("\t%i\t---\n", i);
+		} else if (hash_table[i] == DELETED_NODE) {
+			printf("\t%i\t-<deleted>-\n", i);
+		} else {
+			printf("\t%i\t%s\t%i\n", i, hash_table[i]->name, hash_table[i]->age);
 		}
 	}
 }
@@ -48,22 +54,32 @@ bool hash_table_insert(person *p) {
 	}
 
 	int index = hash(p->name);
-	if (hash_table[index] != NULL) {
-		return false;
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		int try = (index + i) % TABLE_SIZE;
+		if (hash_table[try] == NULL || hash_table[try] == DELETED_NODE) {
+			hash_table[try] = p;
+			return true;
+		}
 	}
-
-	hash_table[index] = p;
-	return true;
+	return false;
 }
 
 person *hash_table_search(char *name) {
 	int index = hash(name);
-	if (hash_table[index] == NULL) {
-		return NULL;
-	}
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		int try = (index + i) % TABLE_SIZE;
 
-	if (strncmp(hash_table[index]->name, name, MAX_NAME) == 0) {
-		return hash_table[index];
+		if (hash_table[try] == NULL) {
+			return NULL;
+		}
+
+		if (hash_table[try] == DELETED_NODE) {
+			continue;
+		}
+
+		if (strncmp(hash_table[try]->name, name, MAX_NAME) == 0) {
+			return hash_table[try];
+		}
 	}
 
 	return NULL;
@@ -71,31 +87,39 @@ person *hash_table_search(char *name) {
 
 person * hash_table_delete(char *name) {
 	int index = hash(name);
-	if (hash_table[index] == NULL) {
-		return NULL;
-	}
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		int try = (index + i) % TABLE_SIZE;
 
-	if (strncmp(hash_table[index]->name, name, MAX_NAME) == 0) {
-		person *p = hash_table[index];
-		hash_table[index] = NULL;
-		return p;
-	}
+		if (hash_table[try] == NULL) {
+			return NULL;
+		}
 
+		if (hash_table[try] == DELETED_NODE) {
+			continue;
+		}
+
+		if (strncmp(hash_table[try]->name, name, MAX_NAME) == 0) {
+			person* p = hash_table[try];
+			hash_table[try] = DELETED_NODE;
+			return p;
+		}
+	}
 	return NULL;
 }
 
 
 int main() {
 	init_hash_table();
-	print_table();
 
 	person john = {.name="John", .age=20};
+	person test = {.name="Jhon", .age=20};
 	person tom = {.name="Tom", .age=30};
 	person jane = {.name="Jane", .age=40};
 	person peter = {.name="Peter", .age=50};
 	person isaac = {.name="Isaac", .age=60};
 
 	hash_table_insert(&john);
+	hash_table_insert(&test);
 	hash_table_insert(&tom);
 	hash_table_insert(&jane);
 	hash_table_insert(&peter);
@@ -113,5 +137,6 @@ int main() {
 	if (hash_table_delete("John") != NULL) {
 		printf("Deleted John\n");
 	}
+	print_table();
 	return 0;
 }
